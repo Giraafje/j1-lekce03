35bcf286dc34cfcac899e4e4a113fdcc6467e588
Lenka Erbenova <lenka.erbenova79@gmail.com>
2025-04-15 20:36:28 +0200

    Three methods with parameters created and used.

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,6 +2,8 @@ package cz.czechitas.turtle;
 
 import dev.czechitas.java1.turtle.engine.Turtle;
 
+import java.awt.*;
+
 public class HlavniProgram {
     private Turtle zofka;
 
@@ -11,8 +13,39 @@ public class HlavniProgram {
 
     public void start() {
         zofka = new Turtle();
-
-        //TODO Tady bude kód pro kreslení želví grafiky.
+        zofka.setPenWidth(5);
+        //drawColoredSquare(100, Color.RED);
+        //drawColoredBlossom(80, Color.ORANGE);
+        drawPolygon(8);
     }
 
+    public void drawColoredSquare(int sideLength, Color penColor) {
+        zofka.setPenColor(penColor);
+        for (int i = 0; i < 4; i++) {
+            zofka.move(sideLength);
+            zofka.turnLeft(90);
+        }
+    }
+
+    public void drawColoredBlossom(int sideLength, Color penColor) {
+        zofka.setPenColor(penColor);
+        for (int i = 0; i < 18; i++) {
+            for (int j = 0; j < 4; j++) {
+                zofka.move(sideLength);
+                zofka.turnLeft(90);
+            }
+            zofka.turnLeft(20);
+        }
+        zofka.penUp();
+        zofka.move(sideLength * 2);
+    }
+
+    public void drawPolygon(int countOfAngles) {
+        double sideLength = (double) 200 / countOfAngles;
+        double angle = 180-(180*(1 - (double) 2 / countOfAngles));
+        for (int i = 0; i < countOfAngles; i++) {
+            zofka.move(sideLength);
+            zofka.turnLeft(angle);
+        }
+    }
 }
